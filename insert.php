5679321d7f9d70d8b6e0bfa8b4e19d81d72f594d
Diana Chau <dianac@fb.com>
2017-08-29 23:45:19 -0700

    Initial commit
    Version 0 of the website!

---
+++ insert.php
@@ -0,0 +1,33 @@
+<?php
+
+  // connect to the database
+  $con = mysqli_connect('localhost','root','');
+
+  if(!$con)
+  {
+    echo 'Not Connected To Server';
+  }
+
+  if(!mysqli_select_db($con,'websitedb'))
+  {
+    echo 'Database Not Selected';
+  }
+
+  $Username = $_POST['username']
+  $Song = $_POST['song']
+  $Comment = ['comment']
+
+  $sql = "INSERT INTO websitedb (Username, Song, Comment) VALUES ('$Name', '$Song', '$Comment')";
+
+  if(!mysqli_query($con,$sql))
+  {
+    echo 'Not Inserted';
+  }
+  else
+  {
+    echo 'Inserted';
+  }
+
+  header("refresh:2; url=index.html");
+
+?>
