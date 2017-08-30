e3a312b5952c2c7204c6167a4e725d4c93a7325d
Diana Chau <dianac@fb.com>
2017-08-29 23:59:28 -0700

    Make heroku load website.

--- insert.php
+++ insert.php
@@ -1,33 +1,34 @@
 <?php
+  include_once('index.html')
 
-  // connect to the database
-  $con = mysqli_connect('localhost','root','');
-
-  if(!$con)
-  {
-    echo 'Not Connected To Server';
-  }
-
-  if(!mysqli_select_db($con,'websitedb'))
-  {
-    echo 'Database Not Selected';
-  }
-
-  $Username = $_POST['username']
-  $Song = $_POST['song']
-  $Comment = ['comment']
-
-  $sql = "INSERT INTO websitedb (Username, Song, Comment) VALUES ('$Name', '$Song', '$Comment')";
-
-  if(!mysqli_query($con,$sql))
-  {
-    echo 'Not Inserted';
-  }
-  else
-  {
-    echo 'Inserted';
-  }
-
-  header("refresh:2; url=index.html");
+  // // connect to the database
+  // $con = mysqli_connect('localhost','root','');
+  //
+  // if(!$con)
+  // {
+  //   echo 'Not Connected To Server';
+  // }
+  //
+  // if(!mysqli_select_db($con,'websitedb'))
+  // {
+  //   echo 'Database Not Selected';
+  // }
+  //
+  // $Username = $_POST['username']
+  // $Song = $_POST['song']
+  // $Comment = ['comment']
+  //
+  // $sql = "INSERT INTO websitedb (Username, Song, Comment) VALUES ('$Name', '$Song', '$Comment')";
+  //
+  // if(!mysqli_query($con,$sql))
+  // {
+  //   echo 'Not Inserted';
+  // }
+  // else
+  // {
+  //   echo 'Inserted';
+  // }
+  //
+  // header("refresh:2; url=index.html");
 
 ?>
